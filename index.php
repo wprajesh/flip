0a97a3ae98000301cad0949dc762217b092355a9
wprajesh <rajeshit0610@gmail.com>
2015-07-22 02:09:35 +0530

    not allow default on ready

--- index.php
+++ index.php
@@ -233,6 +233,7 @@ $out = '';
         }
 
         window.onPlayerReady = function(e) {
+            e.preventDefault();
           if(typeof window.orientation == 'undefined'){
             console.log('mobile');
              // player.playVideo();
